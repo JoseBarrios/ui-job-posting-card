7e17f75f43147c69eb90e482df17939fdc20946d
Jose Barrios <jose@cognilab.com>
2017-07-15 12:00:47 -0700

    Solved issue with paths, got rid of SystemJS in favor of webpack

---
+++ dist/view-controller.js
@@ -0,0 +1,32 @@
+'use strict'
+let currentDocument = document.currentScript.ownerDocument;
+
+class JobPostingCardViewController extends HTMLElement{
+
+  constructor(){
+    super();
+    const template = currentDocument.querySelector('#view').content.cloneNode(true);
+    this.appendChild(template);
+    this.dataController = new JobPosting();
+    console.log("It's alive:", this.dataController)
+  }
+
+  ///STANDARD
+  connectedCallback() {
+    console.log('connected');
+  }
+
+  disconnectedCallback() {
+    console.log('disconnected');
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    console.log('attributeChanged');
+  }
+
+  adoptedCallback(){
+    console.log('adoptedCallback');
+  }
+}
+
+window.customElements.define('ui-job-posting-card', JobPostingCardViewController);
