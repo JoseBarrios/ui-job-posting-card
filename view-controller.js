952ff9c32073f54ecd9d6e27a5cf296ba4d01021
Jose Barrios <jose@cognilab.com>
2017-07-15 06:41:58 -0700

    Basic UI component

---
+++ view-controller.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const lodashURL = '../node_modules/lodash/lodash.js';
+const dataControllerURL = '../node_modules/@josebarrios/thing/index.js';
+
+let currentDocument = document.currentScript.ownerDocument;
+
+let options = {};
+options.map = {};
+options.map.lodash = lodashURL;
+SystemJS.config(options)
+SystemJS.import(dataControllerURL).then(JobPostingDataController => {
+
+  console.log('THING', JobPostingDataController)
+
+  //UIJobPostingController
+  class JobPostingCardViewController extends HTMLElement{
+
+    constructor(){
+      super();
+      const template = currentDocument.querySelector('#view').content.cloneNode(true);
+      this.appendChild(template);
+      console.log('HELLO')
+    }
+
+    ///STANDARD
+    connectedCallback() {
+      console.log('connected');
+    }
+
+    disconnectedCallback() {
+      console.log('disconnected');
+    }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+      console.log('attributeChanged');
+    }
+
+    adoptedCallback(){
+      console.log('adoptedCallback');
+    }
+  }
+
+  window.customElements.define('ui-job-posting-card', JobPostingCardViewController);
+})
